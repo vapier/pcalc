4ab549fb6ec1497dba79d3a3de51c0d728137286
Mike Frysinger <vapier@gmail.com>
2009-12-13 06:18:32 +0000

    switch to 64bit output

--- convert.h
+++ convert.h
@@ -47,6 +47,7 @@
 
 /* -------- Protos ------------------------------------------------------- */
 
+unsigned long long hextoll(char *str);
 unsigned long   hextol(char *str);
 unsigned long   bintol(char *str);
 unsigned long   otol(char *str);
@@ -54,7 +55,7 @@ unsigned int    hextoi(char *str, int lim);
 unsigned int    dectoi(char *str, int lim);
 unsigned int    hexlen(char *str);
 unsigned int    declen(char *str);
-void    long_to_bin_str(unsigned long num, char *str);
+void    long_to_bin_str(unsigned long long num, char *str);
 
 /* -------- Declarations: ------------------------------------------------ */
 
