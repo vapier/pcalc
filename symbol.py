9daed477e309c075f1c4046a40b8d016b285223b
Mike Frysinger <vapier@gmail.com>
2026-08-10 00:06:29 +0545

    tweak helper output to match C source exactly
    Should make copying & pasting & updating faster.

--- symbol.py
+++ symbol.py
@@ -1,13 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """Quickly generate constants."""
 
 from mpmath import mp
 
 
+def p(s, n):
+    s = f'"{s}",'
+    print(f"        {{{s:8} {n}L}},")
+
 mp.dps = 100
-print("PI", mp.pi)
-print("E", mp.e)
-print("GAMMA", mp.euler(0))
-print("DEG", 180 / mp.pi)
-print("PHI", mp.phi)
+p("PI", mp.pi)
+p("E", mp.e)
+p("GAMMA", mp.euler(0))
+p("DEG", 180 / mp.pi)
+p("PHI", mp.phi)
